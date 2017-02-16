6b2b619debff4ed6df346ee8546902a3dfd989e5
joeseiden <joe.seiden214@gmail.com>
2017-02-16 11:30:48 -0800

    Create Users/Sessions controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def log_in_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def already_logged_in
+    if logged_in?
+      current_user.errors[:already] << "logged in!"
+      flash[:errors] = current_user.errors.full_messages
+      redirect_to root_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = self.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+  def self.ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
